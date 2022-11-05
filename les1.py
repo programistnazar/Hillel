20dd6d22533d3af6aa4714c67e1dfcf5231e361a
Nazar_Vovchanskyi <devionmorf@gmail.com>
2022-11-05 23:58:21 +0200

    home task 17

--- les1.py
+++ les1.py
@@ -1,19 +1,16 @@
- def my_decorator(a_function):
-     def wraper():
-         print('Before func')
-         a_function()
-         print('esd')
-
-
-      return wraper
-
-
-
-
-
-
-
-
+line1 = input("введите строку 1: ") + "\n"
+line2 = input("введите строку 2: ") + "\n"
+line3 = input("введите строку 3: ") + "\n"
+line4 = input("введите строку 4: ") + "\n"
+list_line = [line1, line2, line3, line4]
+with open("test12.txt", "w") as f:
+    for index, item in enumerate(list_line):
+        if index <= 1:
+            f.write(item)
+with open("test12.txt", "a") as f:
+    for index, item in enumerate(list_line):
+        if index > 1:
+            f.write(item)
 
 
 
