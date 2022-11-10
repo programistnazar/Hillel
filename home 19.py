0a07f3d531fc204a12ba364108bc3a6ef524f2a1
Nazar_Vovchanskyi <devionmorf@gmail.com>
2022-11-10 22:40:19 +0200

    refactor 32

--- home 19.py
+++ home 19.py
@@ -1,3 +1,24 @@
 # Прочитать сохранённый json-файл из задания №17 и записать данные
 # на диск в csv-файл, первой строкой которого озаглавив каждый
 # столбец и добавив новый столбец “телефон”.
+
+import json
+import csv
+
+with open("task.json", 'r') as f:
+    output_dict = json.load(f)
+
+
+name_fields = ['Number', 'Name', 'Age', 'Phone']
+phone = [1094345353, 342342, 43432, 343434, 4334]
+
+with open('task_1.csv', 'w') as f:
+    file_writer = csv.writer(f)
+    counter = 0
+    for key, value in (output_dict.items()):
+        value.insert(0, key)
+        value.insert(4, phone[counter])
+        if counter == 0:
+            file_writer.writerow(name_fields)
+        counter += 1
+        file_writer.writerow(value)
