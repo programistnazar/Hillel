8d52ebf701c7aabbf6d4917673fa4acfca6a5a5c
Nazar_Vovchanskyi <devionmorf@gmail.com>
2022-11-11 23:14:04 +0200

    refactor 32231

---
+++ home 20.py
@@ -0,0 +1,25 @@
+# Прочитать сохранённый csv-файл из задания №18 и сохранить данные
+# в excel-файл, кроме возраста – столбец с этими данными не нужен.
+
+import csv
+import openpyxl
+
+with open('task_1.csv', 'r') as f:
+    file_reader = list(csv.reader(f))
+
+wb = openpyxl.Workbook()
+wb.create_sheet(title='Первый лист', index=0)
+wb.remove(wb['Sheet'])
+sheet = wb['Первый лист']
+for items, row in enumerate(file_reader):
+    for col_index, value in enumerate(row):
+        if col_index < 2:
+            cell = sheet.cell(row=items+1, column=col_index+1)
+            cell.value = value
+        if col_index > 2:
+            cell = sheet.cell(row=items+1, column=col_index)
+            cell.value = value
+
+wb.save('task_2.xlsx')
+
+
